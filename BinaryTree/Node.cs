b9389547f0645be95ad8bfcaa0a78797eecd6595
Akash-Patil-12 <patilakash121212@gmail.com>
2021-08-09 22:12:21 +0530

    [Akash] Add : Implemented created a bst by adding 56,30 and 70

---
+++ BinaryTree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree
+{
+    public class Node<T> where T : IComparable
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public Node()
+        {
+
+        }
+        public Node(T data)
+        {
+            this.Data = data;
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            Node<int> root = new Node<int>();
+            tree.Insert(56);
+            tree.Insert(30);
+            tree.Insert(70);
+            tree.DisplayTree();
+
+        }
+    }
+}
